fd44376ff154857be67039cb2b7c2a1457c66d05
Arya <aryakaml24@gmail.com>
2024-12-27 00:35:01 +0700

    update dashboard and library auth

---
+++ resources/views/components/card/index.blade.php
@@ -0,0 +1,7 @@
+<div {{ $attributes->merge([
+        'class' => 'bg-white shadow-sm border border-zinc-900 rounded-lg'
+    ]) }}">
+
+    {{ $slot }}
+
+</div>
---
+++ resources/views/components/container.blade.php
@@ -0,0 +1,6 @@
+<div class="max-w-7xl mx-auto py-12 sm:px-6 lg:px-10">
+
+
+    {{ $slot }}
+
+</div>
